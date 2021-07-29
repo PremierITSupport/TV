91bc9b9f09d9784aae682476e7edaafc579f2481
PremierITSupport <it.support@premierag.com>
2021-07-29 10:39:20 -0400

    Create WR_local_install2.bat

---
+++ WR_local_install2.bat
@@ -0,0 +1,4 @@
+msiexec.exe /i "C:\Temp\TV\TeamViewer_Host.msi" /qn SETTINGSFILE="C:\Temp\TV\TVoptions.tvopt" DESKTOPSHORTCUTS=0 APITOKEN=12313699-uz1fz2ldK9ixS8wk6bL1 CUSTOMCONFIGID=66way6k
+ping -n 31 127.0.0.1>nul
+"C:\Program Files (x86)\TeamViewer\TeamViewer.exe" assign --api-token 12313699-uz1fz2ldK9ixS8wk6bL1 [--group-id g186343317 --grant-easy-access --reassign]
+ping -n 15 127.0.0.1>nul
